4b966f47cb255afab4a6e30547f5260b32b05beb
aveleda <41075932+aveleda@users.noreply.github.com>
2022-04-14 15:32:47 -0300

    Uncommented apt-get

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ cp -p spooky2_reader.png ~/.local/share/icons
 chmod 755 ~/.local/bin/spooky2_reader.py
 USER=`whoami`
 sed 's/USER/'"$USER"'/g' spooky2_reader.desktop.dist > ~/Desktop/spooky2_reader.desktop
-#sudo apt-get install python3-tk
+sudo apt-get install python3-tk
 echo "Done."
